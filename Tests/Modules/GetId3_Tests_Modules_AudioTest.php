3d0496c97c811a87178417a89231656a0844bec2
Phansys <phansys@gmail.com>
2012-08-28 16:26:55 -0300

    Added "Tests/Fixtures" directory. Updated test cases "Tests/Modules/GetId3_Tests_Modules_AudioTest". Added "Tests/autoload.php" as test autoloader.

--- Tests/Modules/GetId3_Tests_Modules_AudioTest.php
+++ Tests/Modules/GetId3_Tests_Modules_AudioTest.php
@@ -2,8 +2,38 @@
 
 class GetId3_Tests_Modules_AudioTest extends PHPUnit_Framework_TestCase
 {
-    public function testSample()
+    protected static $mp3File;
+    protected static $class;
+    
+    public function __construct()
+    {
+        self::$mp3File = __DIR__ . '/../Fixtures/mp3demo.mp3';
+        self::$class = 'GetId3_GetId3';
+    }
+    
+    public function testFile()
+    {        
+        $this->assertTrue(file_exists(self::$mp3File) && is_readable(self::$mp3File));
+    }
+    
+    public function testClass()
+    {        
+        $this->assertTrue(class_exists(self::$class));
+        $rc = new ReflectionClass(self::$class);
+        $this->assertTrue($rc->hasProperty('option_md5_data') && $rc->hasProperty('option_md5_data_source') && $rc->hasProperty('encoding'));
+        $this->assertTrue($rc->hasMethod('analyze'));
+        $rm = new ReflectionMethod(self::$class, 'analyze');
+        $this->assertTrue($rm->isPublic());        
+    }
+    
+    public function testReadMp3()
     {       
-        $this->assertNull(null);
+        $getId3 = new GetId3_GetId3();
+		$getId3->option_md5_data        = true;
+		$getId3->option_md5_data_source = true;
+		$getId3->encoding               = 'UTF-8';		
+		$audio = $getId3->analyze(self::$mp3File);	
+        $this->assertTrue(!isset($audio['error']));
+        $this->assertTrue(isset($audio['audio']['dataformat']) && $audio['audio']['dataformat'] == 'mp3');
     }
 }
