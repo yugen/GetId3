e05664b5711e3c9d5b033c252fdbda18ee7b1849
Phansys <phansys@gmail.com>
2012-10-24 12:17:51 -0300

    Added wav audio reading tests.

--- Tests/Modules/AudioTest.php
+++ Tests/Modules/AudioTest.php
@@ -7,11 +7,13 @@ use GetId3\GetId3Core;
 class AudioTest extends \PHPUnit_Framework_TestCase
 {
     protected static $mp3File;
+    protected static $wavFile;
     protected static $class;
 
     protected function setUp()
     {
         self::$mp3File = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'mp3demo.mp3';
+        self::$wavFile = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'Yamaha-SY35-Buzzy-Synth-Lead-C4.wav';
         self::$class = 'GetId3\\GetId3Core';
 
         if (!class_exists(self::$class)) {
@@ -25,7 +27,7 @@ class AudioTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($rm->isPublic());
     }
 
-    public function testFile()
+    public function testMp3File()
     {
         $this->assertTrue(file_exists(self::$mp3File) && is_readable(self::$mp3File));
     }
@@ -39,5 +41,23 @@ class AudioTest extends \PHPUnit_Framework_TestCase
 		$audio = $getId3->analyze(self::$mp3File);
         $this->assertTrue(!isset($audio['error']));
         $this->assertTrue(isset($audio['audio']['dataformat']) && $audio['audio']['dataformat'] == 'mp3');
+    }
+    
+    public function testWavFile()
+    {
+        $this->assertTrue(file_exists(self::$wavFile) && is_readable(self::$wavFile));
+    }
+    
+    public function testReadWav()
+    {
+        $getId3 = new GetId3Core();
+		$getId3->option_md5_data        = true;
+		$getId3->option_md5_data_source = true;
+		$getId3->encoding               = 'UTF-8';
+		$audio = $getId3->analyze(self::$wavFile);
+        $this->assertTrue(!isset($audio['error']));
+        $this->assertTrue(isset($audio['audio']['dataformat']) && $audio['audio']['dataformat'] == 'wav');
+        $this->assertTrue(isset($audio['audio']['codec']) && $audio['audio']['codec'] == 'Pulse Code Modulation (PCM)');
+        $this->assertTrue(isset($audio['audio']['bitrate']) && $audio['audio']['bitrate'] == 1411200);
     }
 }
